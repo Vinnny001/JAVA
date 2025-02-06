9734e3cdb08037cfa72dc78ae9f95f7ce7130c1c
Vincent Ogweno <ogwenovincent00@gmail.com>
2025-02-06 11:44:32 +0300

    Included a for loop.
    The loop prints 5 random numbers.

--- random_numbers.java
+++ random_numbers.java
@@ -1,4 +1,12 @@
 import java.util.Random;
 public class random_numbers {
-    
+    public static void main(String [] args){
+        Random random= new Random();
+        for(int i=0; i<5; i++){
+            int luck= random.nextInt(10)+1;
+
+            System.out.println(luck);
+
+        }
+    }
 }
